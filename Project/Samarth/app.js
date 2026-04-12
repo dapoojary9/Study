ba63ee01345565cf03ee3e4f71989c9a349aa65f
unknown <dapoojary9@gmail.com>
2026-04-12 23:32:35 +0530

    first commit

---
+++ Project/Samarth/app.js
@@ -0,0 +1,31 @@
+let imgs = document.querySelectorAll('.main-slider ul img')
+let prev_btn = document.querySelector('.control-prev')
+let next_btn = document.querySelector('.control-next')
+
+let n = 0;
+
+function changeSlider(){
+    for(let i=0;i<imgs.length;i++){
+        imgs[i].style.display = "none"
+    }
+    imgs[n].style.display = "block"
+}
+
+changeSlider()
+
+prev_btn.addEventListener('click' , (e)=>{
+    if(n>0){
+        n--;
+    }else{
+        n = imgs.length-1;
+    }
+    changeSlider()
+})
+next_btn.addEventListener('click' , (e)=>{
+    if(n<imgs.length-1){
+        n++;
+    }else{
+        n = 0;
+    }
+    changeSlider()
+})
